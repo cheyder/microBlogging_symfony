d13e4d70d01ed2c8dcb7e7ec614d496fb6699255
c.heyder@powercloud.de <c.heyder@powercloud.de>
2020-10-22 18:09:09 +0200

    Write bin/console command

---
+++ src/Command/HelloCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+
+use App\Service\Greeting;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HelloCommand extends Command
+{
+    /**
+     * @var Greeting
+     */
+    private $greeting;
+
+    public function __construct(Greeting $greeting)
+    {
+        $this->greeting = $greeting;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('app:say-hello')
+            ->setDescription('Says hello to the user')
+            ->addArgument('name', InputArgument::REQUIRED);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        $output->writeln([
+            'Hello from the app',
+            '==================',
+            ''
+        ]);
+        $output->writeln($this->greeting->greet($name));
+    }
+}
