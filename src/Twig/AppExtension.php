758fdb5c6f4ca56fb8b0b57619d3e2469a922e5d
c.heyder@powercloud.de <c.heyder@powercloud.de>
2020-11-09 17:28:54 +0100

    Two ways of setting global parameters for twig

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,10 +4,25 @@
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements GlobalsInterface
 {
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * AppExtension constructor.
+     * @param string $locale
+     */
+    public function __construct(string $locale)
+    {
+        $this->locale = $locale;
+    }
+
     /**
      * @return array|TwigFilter[]
      */
@@ -15,6 +30,16 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigFilter('price', [$this, 'priceFilter'])
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getGlobals(): array
+    {
+        return [
+            'locale' => $this->locale
         ];
     }
 
