0bd2458309ba5570f73ef5215385c36ebf29d940
c.heyder@powercloud.de <c.heyder@powercloud.de>
2020-12-03 16:42:00 +0100

    Add user login template, controller and configs

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        return $this->render(
+            'security/login.html.twig',
+            [
+                'last_username' => $authenticationUtils->getLastUsername(),
+                'error' => $authenticationUtils->getLastAuthenticationError()
+            ]
+        );
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        //this method remains empty, because symfony take care of logging out, it only needs a route specified
+    }
+}
